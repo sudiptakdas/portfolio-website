93caebbc80b3229021173d5d7510019eca862f9a
Sudipta Kumar Das <sudiptadas2000k@gmail.com>
2024-01-28 01:08:02 +0530

    main:added Title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Recardo Personal Webside",
+  title: "Recardo Personal Website",
   description: "Full Stack developer",
 };
 
