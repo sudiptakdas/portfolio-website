632607cccbbec29f74447dde18e2cf73748b100b
Sudipta Kumar Das <sudiptadas2000k@gmail.com>
2024-02-01 23:43:37 +0530

    main:Added text color in project

--- components/projects.tsx
+++ components/projects.tsx
@@ -4,7 +4,7 @@ import SectionHeading from './section-heading';
 export default function Projects() {
   return (
     <section>
-      <SectionHeading> My Projects</SectionHeading>
+      <SectionHeading > <h1 className=' text-gray-900'>My Projects</h1></SectionHeading>
     </section>
   );
 }
