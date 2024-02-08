ee0cc7186c136fffd637d682892ca4b2c8b978df
Sudipta Kumar Das <sudiptadas2000k@gmail.com>
2024-02-08 22:02:51 +0530

    main:added experience

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import SectionDivider from '@/components/section-divider';
 import About from '@/components/about';
 import Projects from '@/components/projects';
 import Skills from '@/components/skills';
+import Experience from '@/components/experience';
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         <About />
         <Projects />
         <Skills />
+        <Experience />
       </div>
     </main>
   );
